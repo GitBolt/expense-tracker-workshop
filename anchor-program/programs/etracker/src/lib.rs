c7f61a75bb285c5d3fe0d725eaa180af020bef44
Madhur Dixit <dmadhur6@gmail.com>
2023-05-13 03:06:41 +0530

    program update

--- anchor-program/programs/etracker/src/lib.rs
+++ anchor-program/programs/etracker/src/lib.rs
@@ -1,43 +1,44 @@
 use anchor_lang::prelude::*;
 use anchor_lang::solana_program::{self,system_program, sysvar::rent::Rent,};
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+// declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("Dt1Cu5jz7UMzLFgXPNyYcP8jCcySYgLW5QXpubjKvvm3");
 
 
 
 #[program]
 pub mod etracker {
-    use std::string;
 
     use super::*;
 
-    pub fn initialize_expense(ctx: Context<InitializeExpense>,_id : String,_mname : String,_amount : u64) -> Result<()> {
+    pub fn initialize_expense(ctx: Context<InitializeExpense>,id : String,mname : String,amount : u64) -> Result<()> {
         
         let expense_account = &mut ctx.accounts.expense_account;
-        let integer_id = _id.parse::<u64>().unwrap();
+        let integer_id = id.parse::<u64>().unwrap();
         let string_id = integer_id.to_string();
 
-        require_eq!(string_id, _id);
+        require_eq!(string_id, id);
 
         expense_account.id = integer_id;
-        expense_account.mname = _mname;
-        expense_account.amount = _amount;
+        expense_account.mname = mname;
+        expense_account.amount = amount;
+        expense_account.owner = *ctx.accounts.authority.key;
         expense_account.bump = *ctx.bumps.get("expense_account").unwrap();
 
         Ok(())
     }
 
     
-    pub fn modify_expense(ctx: Context<ModifyExpense>,_id : String,_mname : String,_amount : u64) -> Result<()> {
+    pub fn modify_expense(ctx: Context<ModifyExpense>,id : String,mname : String,amount : u64) -> Result<()> {
         
         let expense_account = &mut ctx.accounts.expense_account;
-        expense_account.mname = _mname;
-        expense_account.amount = _amount;
+        expense_account.mname = mname;
+        expense_account.amount = amount;
 
         Ok(())
     }
 
-    pub fn delete_expense(ctx: Context<DeleteExpense>,_id : String) -> Result<()> {
+    pub fn delete_expense(ctx: Context<DeleteExpense>,id : String) -> Result<()> {
 
         Ok(())
     }
@@ -46,7 +47,7 @@ pub mod etracker {
 }
 
 #[derive(Accounts)]
-#[instruction(_id : String)]
+#[instruction(id : String)]
 pub struct InitializeExpense<'info> {
 
     #[account(
@@ -60,10 +61,11 @@ pub struct InitializeExpense<'info> {
         payer = authority,
         space = 8 
             + 8 // id
+            + 32 // owner
             + (4 + 12) // merchant name
             + 8 // amount
             + 1, // bump
-        seeds = [b"expense".as_ref(),authority.key().as_ref(),_id.as_ref()], 
+        seeds = [b"expense".as_ref(),authority.key().as_ref(),id.as_ref()], 
         bump
     )]
     pub expense_account: Account<'info, ExpenseAccount>,
@@ -76,7 +78,7 @@ pub struct InitializeExpense<'info> {
 }
 
 #[derive(Accounts)]
-#[instruction(_id : String)]
+#[instruction(id : String)]
 pub struct ModifyExpense<'info> {
 
     #[account(
@@ -87,7 +89,7 @@ pub struct ModifyExpense<'info> {
 
     #[account(
         mut,
-        seeds = [b"expense".as_ref(),authority.key().as_ref(),_id.as_ref()], 
+        seeds = [b"expense".as_ref(),authority.key().as_ref(),id.as_ref()], 
         bump=expense_account.bump
     )]
     pub expense_account: Account<'info, ExpenseAccount>,
@@ -100,7 +102,7 @@ pub struct ModifyExpense<'info> {
 }
 
 #[derive(Accounts)]
-#[instruction(_id : String)]
+#[instruction(id : String)]
 pub struct DeleteExpense<'info> {
 
     #[account(
@@ -112,7 +114,7 @@ pub struct DeleteExpense<'info> {
     #[account(
         mut,
         close = authority,
-        seeds = [b"expense".as_ref(),authority.key().as_ref(),_id.as_ref()], 
+        seeds = [b"expense".as_ref(),authority.key().as_ref(),id.as_ref()], 
         bump=expense_account.bump
     )]
     pub expense_account: Account<'info, ExpenseAccount>,
@@ -129,6 +131,7 @@ pub struct DeleteExpense<'info> {
 #[derive(Default)]
 pub struct ExpenseAccount {
     pub id: u64,
+    pub owner: Pubkey,
     pub mname: String,
     pub amount: u64,
     pub bump: u8,
