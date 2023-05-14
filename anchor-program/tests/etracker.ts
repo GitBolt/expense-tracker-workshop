0ad61438b145669fb7c800f3bb41d116d72ba211
GitBolt <saabisakhtar@gmail.com>
2023-05-15 01:44:54 +0530

    fix: tests

--- anchor-program/tests/etracker.ts
+++ anchor-program/tests/etracker.ts
@@ -2,9 +2,8 @@ import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { Etracker } from "../target/types/etracker";
 
-describe("Expense Tracker", async() => {
+describe("Expense Tracker", async () => {
 
-  // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
   const provider = anchor.AnchorProvider.local();
 
@@ -14,62 +13,45 @@ describe("Expense Tracker", async() => {
 
   let merchant_name = "test";
   let amount = 100;
-  let id = "1"
+  let id = "10";
 
   let merchant_name2 = "test 2";
   let amount2 = 200;
 
-  let [expense_account] = await anchor.web3.PublicKey.findProgramAddressSync(
+  let [expense_account] = anchor.web3.PublicKey.findProgramAddressSync(
     [Buffer.from("expense"), wallet.publicKey.toBuffer(), Buffer.from(id)],
     program.programId
   );
 
   it("Initialize Expense", async () => {
-
-    await program.methods.initializeExpense(
-      id,
-      merchant_name,
-      new anchor.BN(amount)
-    ).accounts({
-      expenseAccount: expense_account,
-      authority: wallet.publicKey,
-    })
-    .rpc();
-
-    let data = await program.account.expenseAccount.fetch(expense_account);
-    console.log(data)
-
+    await program.methods
+      .initializeExpense(id, merchant_name, new anchor.BN(amount))
+      .accounts({
+        expenseAccount: expense_account,
+        authority: wallet.publicKey,
+      })
+      .rpc();
   });
 
   it("Modify Expense", async () => {
     // Add your test here.
 
-    await program.methods.modifyExpense(
-      id,
-      merchant_name2,
-      new anchor.BN(amount2)
-    ).accounts({
-      expenseAccount: expense_account,
-      authority: wallet.publicKey,
-    })
-    .rpc();
-
-    let data = await program.account.expenseAccount.fetch(expense_account);
-    console.log(data)
-
+    await program.methods
+      .modifyExpense(id, merchant_name2, new anchor.BN(amount2))
+      .accounts({
+        expenseAccount: expense_account,
+        authority: wallet.publicKey,
+      })
+      .rpc();
   });
 
   it("Delete Expense", async () => {
-
-    await program.methods.deleteExpense(
-      id
-    ).accounts({
-      expenseAccount: expense_account,
-      authority: wallet.publicKey,
-    })
-    .rpc();
-
+    await program.methods
+      .deleteExpense(id)
+      .accounts({
+        expenseAccount: expense_account,
+        authority: wallet.publicKey,
+      })
+      .rpc();
   });
-
-
 });
